d3a562f3264541a6a6a1d0499247f53e877c447b
Daniil <ralf.danya@mail.ru>
2022-10-22 18:10:30 +0700

    add: carcase admin panel

--- src/app/admin-layout/panel/panel.component.ts
+++ src/app/admin-layout/panel/panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-panel',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private StorageService: StorageService,
+  ) { }
 
   ngOnInit(): void {
+    this.StorageService.getUser();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
-import { ChangeDetectorRef, NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AdminLayoutModule } from './admin-layout/admin-layout.module';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { PublicLayoutModule } from './public-layout/public-layout.module';
-import { PanelComponent } from './admin-layout/panel/panel.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './helpers/request.interceptor';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SharedViewModule } from './shared-views/shared-views.module';
+import { PublicRoutingModule } from './public-layout/public-layout-routing.module';
+import { AdminRoutingModule } from './admin-layout/admin-layout-routing.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PanelComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+
     ReactiveFormsModule,
 
     AdminLayoutModule,
-    PublicLayoutModule
+    PublicLayoutModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true},
