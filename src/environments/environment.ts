a60d76032551a7a2e634d7f9c2f0a6dfdc5486d7
DaniilRalf <ralf.danya@mail.ru>
2023-04-26 22:54:20 +0700

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiBaseUrl: 'http://localhost:8085/',
+  apiBaseUrl: 'http://62.217.182.204:8085/',
   // apiBaseUrl: 'http://localhost:8085/',
   production: false,
 };
